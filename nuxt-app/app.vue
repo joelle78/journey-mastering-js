f50c24880401bef25bde0c8b5ea95d4271eab634
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-08-06 17:29:37 +0200

    Project opzet

---
+++ nuxt-app/app.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <link rel="stylesheet" href="https://use.typekit.net/eyu4tao.css">
+
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+
+  </div>
+</template>

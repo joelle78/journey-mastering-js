528ad2ecde73b1dcb162eb93ece13b892e03e85e
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-08-24 11:57:00 +0200

    test

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,12 +1,17 @@
-<script setup lang="ts">
+<script setup>
 </script>
 
 <template>
   <div>
 
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Anton&family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    </Head>
+
     <NuxtLayout>
       <NuxtPage/>
     </NuxtLayout>
-
   </div>
 </template>

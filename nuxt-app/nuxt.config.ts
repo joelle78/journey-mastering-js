a946df75451d0090329124a9b0446db8d8078705
joelle78 <96306220+joelle78@users.noreply.github.com>
2024-08-24 12:00:24 +0200

    update

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,10 +1,7 @@
 export default defineNuxtConfig({
-
   devtools: { enabled: true },
 
-
   modules: ['nuxt-graphql-client'],
-
 
   css: [
     'assets/main.css',
@@ -16,9 +13,13 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'nl',
       },
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Anton&family=Jost:ital,wght@0,100..900;1,100..900&display=swap' }
+      ]
     },
   },
-
 
   compatibilityDate: '2024-08-09',
 });
